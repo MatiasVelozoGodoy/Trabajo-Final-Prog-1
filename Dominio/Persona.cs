c887fa165ab5f75331b319781905d90c0e272df2
Matias Velozo <matias.velozo.4c@gmail.com>
2024-06-04 13:30:45 -0300

    agregando nueva persona

---
+++ Dominio/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Persona
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
