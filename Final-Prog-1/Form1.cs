f5ad0947cd5181d09df28c112404d98e8c3eda80
Matias Velozo <matias.velozo.4c@gmail.com>
2024-06-04 14:28:46 -0300

    agregando funciones

--- Final-Prog-1/Form1.cs
+++ Final-Prog-1/Form1.cs
@@ -16,5 +16,45 @@ namespace Final_Prog_1
         {
             InitializeComponent();
         }
+
+        private void btnVerDeuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Su deuda es de xxxx");
+        }
+
+        private void btnPagarDeuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deuda pagada");
+        }
+
+        private void brnCargarDeuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cuanto quiere cargar de deuda");
+        }
+
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            lblDNI.Visible = true;
+            btnAceptar.Visible= true;
+            btnCacelar.Visible= true;
+            btnAgregar.Visible= false;
+            txtContraseña.Visible= true;
+            btnPagarDeuda.Visible = true;
+            btnVerDeuda.Visible = true;
+            btnCargarDeuda.Visible = true;
+        }
+
+        private void btnCacelar_Click(object sender, EventArgs e)
+        {
+            txtContraseña.Clear();
+            lblDNI.Visible = false;
+            btnAceptar.Visible = false;
+            btnCacelar.Visible = false;
+            btnAgregar.Visible = true;
+            txtContraseña.Visible = false;
+            btnPagarDeuda.Visible = false;
+            btnVerDeuda.Visible = false;
+            btnCargarDeuda.Visible = false;
+        }
     }
 }
